1656b195199ad1ead28ece3d0ce32d7d680c7651
Miguel <miguelmayhem92@gmail.com>
2024-02-10 08:38:28 +0100

    flexible data_window

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from virgo_modules.src.re_utils import calculate_cointegration
 from virgo_modules.src.ticketer_source import stock_eda_panel
     
-obj = stock_eda_panel(stock_code = 'PEP', n_days = 15000,data_window = '50y' )
+obj = stock_eda_panel(stock_code = 'PEP', n_days = 15000,data_window = '15y' )
 obj.get_data()
 print(obj.df.shape)
