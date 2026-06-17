13c375533ed405779ff489fb4652c64cfcc0a8c1
miguel <miguelmayhem92@gmail.com>
2026-06-17 21:10:21 +0200

    max quadratic

--- virgo_app/virgo_modules/src/markowitz/markowitz_utils.py
+++ virgo_app/virgo_modules/src/markowitz/markowitz_utils.py
@@ -1,13 +1,17 @@
 from scipy import optimize
 import pandas as pd
 import numpy as np
+from pypfopt.risk_models import risk_matrix
+from pypfopt.efficient_frontier import EfficientFrontier
+
 
 class MarkowitzOptimizer:
-    def __init__(self, data, return_cols, window_cov=10):
+    def __init__(self, data, return_cols, window_cov=10, max_quadratic=False):
         self.data_ = data[["Date"]+return_cols].dropna().sort_values("Date").copy()
         self.window_cov = window_cov
         self.return_cols = return_cols
         self.n_features = len(return_cols)
+        self.max_quadratic = max_quadratic
         
     def execute_markowitz(self):
         cons = {'type':'eq','fun':self._check_sum}
@@ -17,11 +21,21 @@ class MarkowitzOptimizer:
         self.data_[feature_result_names] = np.nan
 
         for i in range(self.window_cov,len(self.data_)):
-            self.cov = self.data_.iloc[i-self.window_cov:i,:][self.return_cols].cov()
             self.returns = self.data_.iloc[i][self.return_cols].values
-            
-            opt_results = optimize.minimize(self._neg_sr, init_guess, constraints=cons, bounds=bounds, method='SLSQP')
-            optimal_weights = opt_results.x
+            self.cov = risk_matrix(
+                prices=self.data_.iloc[i-self.window_cov:i,:][self.return_cols],
+                returns_data=True,
+                method="sample_cov"
+            )
+            if not self.max_quadratic:
+                opt_results = optimize.minimize(self._neg_sr, init_guess, constraints=cons, bounds=bounds, method='SLSQP')
+                optimal_weights = opt_results.x
+            else:
+                # max quadratic will return weights from -1 to 1 (short and long)
+                ef = EfficientFrontier(self.returns, self.cov)
+                weights = ef.max_quadratic_utility(risk_aversion=1,market_neutral=True)
+                weights = dict(weights)
+                optimal_weights = weights.values()
             self.data_.iloc[i,-self.n_features:] = [float(x) for x in optimal_weights]
             
         self.data_[feature_result_names] = self.data_[feature_result_names].astype(float).round(6) 
